5d07afbc7b0ae25a475adc45a6785a1eac7d8fda
dhruvadeep <dhruvadeepmalakar12345@gmail.com>
2024-03-21 22:07:32 +0530

    allocator and deallocator

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Define the capacity of the heap
+#define CAPACITY 1000
+
+char heap[CAPACITY]; // The heap
+
+// heap_alloc: allocate a block of memory of size bytes
+// Allocates a block of size bytes from the heap and returns a pointer to the block
+void *heap_alloc(size_t size) // returns a pointer 
+{
+    return NULL;
+}
+
+// heap_free: free the memory block pointed to by ptr
+// Deallocates the space previously allocated by heap_alloc
+void heap_free(void *ptr) // takes a pointer
+{
+    return NULL;
+}
